5e4045dbc48a5042513f5763359809e2e776af9c
Burak Geylani <burak.geylani@dijitalsahne.com>
2021-10-26 15:28:41 +0300

    🔧 added configuration

---
+++ src/variables.ts
@@ -0,0 +1,21 @@
+// declaration variable type
+let numberType: number
+let stringType: string
+let boolType: boolean
+let stringArray: string[] = ["item1", "item2"]
+let numberArray: number[] = [1, 2, 3, 4]
+
+// initilazition variable
+numberType = 10.5
+stringType = "burak"
+boolType = false
+
+console.warn("numberType", numberType.valueOf())
+console.warn("stringType", stringType.valueOf())
+console.warn("booelanType", boolType.valueOf())
+for (var index in stringArray) {
+  console.warn("StringArray index:", index + "value:", stringArray[index])
+}
+numberArray.forEach((index, value) => {
+  console.warn("NumberArray Index: ", index, "value:", value.valueOf())
+})
