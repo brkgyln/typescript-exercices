68f867529f7d1087aad4e853cb5ebbf0fca8388f
Burak Geylani <burak.geylani@dijitalsahne.com>
2021-10-26 15:44:10 +0300

    ♻️ refactor

--- src/variables.ts
+++ src/variables.ts
@@ -1,21 +1,23 @@
-// declaration variable type
+/** declaration variable type */
+
 let numberType: number
 let stringType: string
 let boolType: boolean
 let stringArray: string[] = ["item1", "item2"]
 let numberArray: number[] = [1, 2, 3, 4]
 
-// initilazition variable
+/** initilazition variable */
 numberType = 10.5
 stringType = "burak"
 boolType = false
 
+/** output */
 console.warn("numberType", numberType.valueOf())
 console.warn("stringType", stringType.valueOf())
 console.warn("booelanType", boolType.valueOf())
 for (var index in stringArray) {
-  console.warn("StringArray index:", index + "value:", stringArray[index])
+  console.warn("StringArray index: ", index + "value: ", stringArray[index])
 }
 numberArray.forEach((index, value) => {
-  console.warn("NumberArray Index: ", index, "value:", value.valueOf())
+  console.warn("NumberArray Index: ", index + "value:", value.valueOf())
 })
